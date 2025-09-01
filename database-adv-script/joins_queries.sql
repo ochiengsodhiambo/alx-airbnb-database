2947d80a145bd47294af0d15a7abf7076124280a
Ochieng Odhiambo <chiengsun@gmail.com>
2025-09-01 22:14:50 +0300

    Update joins_queries.sql
    order by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -23,7 +23,8 @@ SELECT
     r.comment,
     r.created_at
 FROM property p
-LEFT JOIN review r ON p.property_id = r.property_id;
+LEFT JOIN review r ON p.property_id = r.property_id
+    ORDER BY r.rating ASC;
 
 --FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT 
